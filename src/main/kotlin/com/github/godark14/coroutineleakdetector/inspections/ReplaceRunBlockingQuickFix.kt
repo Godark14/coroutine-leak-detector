46140dfb053d239abb4436c750739e6458c60296
Kokou Godwin AGBEMADOKPONOU <cstephen679@gmail.com>
2026-08-15 20:56:52 +0000

    Make runBlocking quick fix context-aware (suggest viewModelScope/lifecycleScope in UI classes)

--- src/main/kotlin/com/github/godark14/coroutineleakdetector/inspections/ReplaceRunBlockingQuickFix.kt
+++ src/main/kotlin/com/github/godark14/coroutineleakdetector/inspections/ReplaceRunBlockingQuickFix.kt
@@ -7,20 +7,32 @@ import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.KtExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
 
-class ReplaceRunBlockingQuickFix : LocalQuickFix {
+class ReplaceRunBlockingQuickFix(
+    private val isLikelyUiCode: Boolean,
+    private val suggestedReplacement: String
+) : LocalQuickFix {
 
     override fun getFamilyName(): String =
-        "Add TODO to review runBlocking usage"
+        if (isLikelyUiCode) {
+            "Add TODO to replace runBlocking with $suggestedReplacement"
+        } else {
+            "Add TODO to review runBlocking usage"
+        }
 
     override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
         val element: PsiElement = descriptor.psiElement
         val expression = element as? KtExpression ?: return
 
-        val factory = KtPsiFactory(project)
-        val comment = factory.createComment(
+        val commentText = if (isLikelyUiCode) {
+            "// TODO: This runBlocking is likely on the main thread here. " +
+                    "Replace with $suggestedReplacement.launch { ... } instead."
+        } else {
             "// TODO: Confirm this runBlocking is not executed on the main/UI thread. " +
                     "If it is, replace it with launch/async on a proper CoroutineScope."
-        )
+        }
+
+        val factory = KtPsiFactory(project)
+        val comment = factory.createComment(commentText)
 
         val parent = expression.parent
         parent.addBefore(comment, expression)
