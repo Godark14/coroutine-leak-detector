2454b875cf7cd81eed7cb84a2664fe371710d422
Kokou Godwin AGBEMADOKPONOU <cstephen679@gmail.com>
2026-08-15 21:03:25 +0000

    Add test coverage for context-aware runBlocking quick fix

--- src/test/kotlin/com/github/godark14/coroutineleakdetector/inspections/RunBlockingUsageInspectionTest.kt
+++ src/test/kotlin/com/github/godark14/coroutineleakdetector/inspections/RunBlockingUsageInspectionTest.kt
@@ -10,4 +10,28 @@ class RunBlockingUsageInspectionTest : BasePlatformTestCase() {
         myFixture.enableInspections(RunBlockingUsageInspection::class.java)
         myFixture.testHighlighting(true, false, false, "RunBlockingUsage.kt")
     }
+
+    fun testSuggestsViewModelScopeReplacementInViewModelContext() {
+        myFixture.enableInspections(RunBlockingUsageInspection::class.java)
+        myFixture.configureByFiles(
+            "RunBlockingInViewModel.kt",
+            "androidx/lifecycle/ViewModel.kt"
+        )
+
+        val highlights = myFixture.doHighlighting()
+        val warning = highlights.firstOrNull {
+            it.description?.contains("runBlocking blocks the calling thread") == true
+        }
+        assertNotNull("Expected the inspection to report a warning", warning)
+
+        myFixture.editor.caretModel.moveToOffset(warning!!.startOffset)
+
+        val quickFix = myFixture.getAvailableIntention(
+            "Add TODO to replace runBlocking with viewModelScope"
+        )
+        assertNotNull(
+            "Expected the context-aware quick fix suggesting viewModelScope",
+            quickFix
+        )
+    }
 }
---
+++ src/test/testData/inspections/androidx/lifecycle/ViewModel.kt
@@ -0,0 +1,3 @@
+package androidx.lifecycle
+
+open class ViewModel
